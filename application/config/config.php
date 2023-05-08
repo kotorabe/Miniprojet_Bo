12bea7a13933b88cc31e49a17ff2806ba92650df
kotorabe <kotorajao@gmail.com>
2023-05-08 21:04:06 +0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Miniprojet_Bo/';
+$config['base_url'] = 'https://miniprojetbo-intelligence-artificielle.up.railway.app/';
 
 /*
 |--------------------------------------------------------------------------
